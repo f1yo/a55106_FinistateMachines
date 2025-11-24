d1f33954d5e3ca2a6b4d8475621bfd6d194a959e
fly <Benylaranjinha@gmail.com>
2025-11-24 22:18:23 +0000

    Patrolling the Perimeter

---
+++ Assets/AI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    State currentState;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+        currentState = new Idle(this.gameObject, agent, anim, player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState = currentState.Process();
+    }
+}
